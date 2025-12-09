928c5d1780eb9194becb34e1615e5ce85524278c
Md Naim <naim.cse2004@gmail.com>
2025-12-09 11:48:53 +0600

    Function inside class

---
+++ phitron/C++ for DSA/String Constroctor C++ Module-6/reverse_in_builtin.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    //    int n;
+    //    cin>>n;
+
+    //    int arr[n];
+
+    //    for(int i=0;i<n;i++){
+    //     cin >> arr[i];
+    //    }
+
+  // sort(arr, arr+n);
+
+    //reverse(arr, arr+n);
+
+    reverse(s.begin(), s.end());
+    //    for(int i=0; i<n; i++){
+    //     cout <<" " <<arr[i] ;
+    //    }
+        cout<<s<<endl;
+    return 0;
+}
