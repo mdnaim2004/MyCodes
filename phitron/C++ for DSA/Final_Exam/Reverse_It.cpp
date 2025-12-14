235ba44fb0889ac76ebdf23c0996a0bc52a3a6fe
Md Naim <naim.cse2004@gmail.com>
2025-12-14 17:35:48 +0600

    hacker rank problem solving

--- phitron/C++ for DSA/Final_Exam/Reverse_It.cpp
+++ phitron/C++ for DSA/Final_Exam/Reverse_It.cpp
@@ -19,9 +19,15 @@ int main() {
         cin >> arr[i].nm >> arr[i].cls >>arr[i].s >> arr[i].id;
     }
 
-    // reverse (s.begin(), s.end());
-    
-    reverse(arr, arr + n);
+    //reverse (s.begin(), s.end());
+
+    int i=0; 
+    int j = n-1;
+    while(i<j){
+        swap(arr[i].s, arr[j].s);
+        i++;
+        j--;
+    }
 
     for(int i=0; i<n; i++){
         cout << arr[i].nm << " " << arr[i].cls << " " << arr[i].s << " " << arr[i].id << endl;
