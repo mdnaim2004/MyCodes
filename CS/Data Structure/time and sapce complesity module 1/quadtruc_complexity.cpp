73cbf710a5bdfade5057a06da03fb10f60e8485d
Md Naim <naim.cse2004@gmail.com>
2025-12-15 23:46:47 +0600

    time complexity

--- CS/Data Structure/time and sapce complesity module 1/quadtruc_complexity.cpp
+++ CS/Data Structure/time and sapce complesity module 1/quadtruc_complexity.cpp
@@ -2,11 +2,33 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
+    int n,m, l;
+    cin>>n >>m >>l;
+
+    //time complwxity of order of O(n*n)
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout << "hello" << endl;
+    //     }
+    // }
+    
+
+    // time complexity of order of O(n*m)
+
+    // for(int i=0; i<n;i++){
+    //     for(int j=0; j<m; j++){
+    //         cout << "hello" << endl;
+    //     }
+    // }
+
+
+    //time complexity of order of O(l*n*m)
+
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << "hello" << endl;
+        for(int j=0; j<m; j++){
+            for(int k=0; k<l; k++){
+                cout << "Hello" << endl;
+            }
         }
     }
     return 0;
