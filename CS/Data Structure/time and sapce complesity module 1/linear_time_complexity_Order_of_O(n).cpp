d73433593ee637cca1c893e0c3d1995de2073fdc
Md Naim <naim.cse2004@gmail.com>
2025-12-15 14:41:06 +0600

    time complexity

---
+++ CS/Data Structure/time and sapce complesity module 1/linear_time_complexity_Order_of_O(n).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    //int arr[n];
+    for(int i=0; i<n; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+    for(int i=0; i<n; i+=2){
+        cout << i << " ";
+    }
+    return 0;
+}
