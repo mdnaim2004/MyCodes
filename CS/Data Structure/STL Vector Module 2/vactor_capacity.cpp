71873c7db1c93babae3f4bedacaaf0e3346f7455
Md Naim <naim.cse2004@gmail.com>
2025-12-18 08:59:03 +0600

    vector capacity

---
+++ CS/Data Structure/STL Vector Module 2/vactor_capacity.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //vector <int> v;
+    //cout << v.capacity() << endl;
+
+    vector<int>v;
+    cout<<v.capacity() <<endl;
+    v.push_back(19);
+    cout << v.capacity() <<endl;
+    v.push_back(20);
+    cout<<v.capacity()<<endl;
+    v.push_back(30);
+    cout << v.capacity() << endl;
+    v.push_back(30);
+    cout << v.capacity() << endl;
+    v.push_back(30);
+    cout << v.capacity() << endl;
+
+    cout<< "Vector size :";
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+    cout << "Vector resize :";
+    v.resize(4);
+    for(int i=0; i<v.size(); i++){
+        cout << v[i]<< " ";
+    }
+    cout << endl;
+    cout <<"increase the vector and set value of vector:";
+
+    v.resize(10,200);
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " " ;
+    }
+
+    return 0;
+}
