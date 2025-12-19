7fbae31cfb57a1afa3b7190f8b843e1406c1504a
Md Naim <naim.cse2004@gmail.com>
2025-12-19 14:49:57 +0600

    add replace and find function

--- CS/Data Structure/STL Vector Module 2/vector_modifires.cpp
+++ CS/Data Structure/STL Vector Module 2/vector_modifires.cpp
@@ -24,14 +24,38 @@ int main() {
 
     //insert multiple
 
-    vector<int> v = {1,2,3,4,5};
+    //vector<int> v = {1,2,3,4,5};
     // vector<int>v2 = {100, 200, 300,};
     // v.insert(v.begin()+3,v2.begin(), v2.end());
 
-    v.erase(v.begin()+2,v.begin()+5);
+    //v.erase(v.begin()+2,v.begin()+5);
 
-    for(int x : v){
-        cout<< x << " ";
+    //vector replace
+
+    //vector<int> v = {1,2,3,4,5,2,6,2,7,8,2};
+    // replace(v.begin(), v.end(), 2, 100);
+
+    //replace(v.begin(), v.end()-1,2,100);
+
+
+    //find the vector of value
+
+    
+    // for(int x : v){
+    //     cout<< x << " ";
+    // }
+
+
+    vector<int> v = {1,2,3,2,4,5,2};
+    //vector<int> :: iterator it = find(v.begin(), v.end(), 5);
+
+    auto it = find(v.begin(), v.end(), 5);
+    if(it == v.end()){
+        cout << "Not Found" << endl;
+    }
+    else{
+        cout << "Found" << endl;
+        cout << *it <<endl;
     }
     return 0;
 }
