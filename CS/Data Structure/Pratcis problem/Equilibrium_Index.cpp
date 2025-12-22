09228b8e260f2e52799c7904f3de8d3f108f8b82
Md Naim <naim.cse2004@gmail.com>
2025-12-22 23:31:44 +0600

    Update code

--- CS/Data Structure/Pratcis problem/Equilibrium_Index.cpp
+++ CS/Data Structure/Pratcis problem/Equilibrium_Index.cpp
@@ -13,10 +13,57 @@ int main() {
         sum += v[i];
     }
 
-    cout << sum << endl;
-    
+    int left = 0;
     for(int i=0; i<n; i++){
-        cout << v[i] <<" ";
+        int right = sum-left-v[i];
+
+        if(left == right){
+            cout << i << endl;
+            return 0;
+        }
+        left += v[i];
     }
+
+    //cout << sum << endl;
+    
+    // for(int i=0; i<n; i++){
+    //     cout << v[i] <<" ";
+    // }
     return 0;
 }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     vector<int> arr(n);
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int totalSum = 0;
+//     for(int x : arr) {
+//         totalSum += x;
+//     }
+
+//     int leftSum = 0;
+//     for(int i = 0; i < n; i++) {
+//         int rightSum = totalSum - leftSum - arr[i];
+
+//         if(leftSum == rightSum) {
+//             cout << i << endl;
+//             return 0;
+//         }
+
+//         leftSum += arr[i];
+//     }
+
+//     //cout << -1 << endl; // equilibrium index না থাকলে
+//     return 0;
+// }
