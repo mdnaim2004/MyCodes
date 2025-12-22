dc656975c398480f485bef9713d604105d2a059f
Md Naim <naim.cse2004@gmail.com>
2025-12-23 00:13:56 +0600

    time complexity O(1)

--- CS/Data Structure/Pratcis problem/G_Summation_from_1_to_N.cpp
+++ CS/Data Structure/Pratcis problem/G_Summation_from_1_to_N.cpp
@@ -1,20 +1,34 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector <int> arr(n);
+
+//     for(int i = 0; i < n; i++){
+//         arr[i] = i + 1;
+//     }
+//     int sum = 0;
+
+//     for(int i = 0; i < n; i++){
+//         sum +=arr[i];
+//     }
+//     cout << sum << endl;
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector <int> arr(n);
 
-    for(int i = 0; i < n; i++){
-        arr[i] = i + 1;
-    }
-    int sum = 0;
-
-    for(int i = 0; i < n; i++){
-        sum +=arr[i];
-    }
+    int sum = (n*(n+1))/2;
     cout << sum << endl;
-
     return 0;
 }
+
