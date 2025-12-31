89498673d5655c3f96405584f17665e0ca22f3bc
Md Naim <naim.cse2004@gmail.com>
2025-12-31 18:27:13 +0600

    add comment

--- CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/input.c
+++ CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/input.c
@@ -1,9 +1,11 @@
+
+#include<stdio.h>
 int main(){
-    int num1;
-    float num2;
-    char ch;
+    int num1; //intiger type variable
+    float num2; //floating type variable
+    char ch; //character type variable
 
-    scanf("%d %f %c", &num1, &num2, &ch);
+    scanf("%d %f %c", &num1, &num2, &ch); // user input all type variable
 
-    printf("num1 = %d\nnum2 = %f\nchar = %c\n", num1, num2, ch);
+    printf("num1 = %d\nnum2 = %f\nchar = %c\n", num1, num2, ch); //print the all value
 }
