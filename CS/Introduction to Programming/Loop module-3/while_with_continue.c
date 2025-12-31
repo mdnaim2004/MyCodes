cd11b2670b70e972e6f43b9e26a1414e71618783
Md Naim <naim.cse2004@gmail.com>
2025-12-31 19:51:58 +0600

    while loop with condition

--- CS/Introduction to Programming/Loop module-3/while_with_continue.c
+++ CS/Introduction to Programming/Loop module-3/while_with_continue.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-    int i=1;
-    while(i<=10){
+    int i=1; //inmitilization
+    while(i<=10){ // consition
         if(i == 6){
             continue;
         }
         printf("%d\n", i);
-        i++;
+        i++;  //increment or decrement
     }
     return 0;
 }
