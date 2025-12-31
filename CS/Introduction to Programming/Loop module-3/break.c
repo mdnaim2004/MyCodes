faecb6aca50e1ed2e57839474fb8d2e51e667d50
Md Naim <naim.cse2004@gmail.com>
2025-12-31 20:31:33 +0600

    New code

--- CS/Introduction to Programming/Loop module-3/break.c
+++ CS/Introduction to Programming/Loop module-3/break.c
@@ -3,7 +3,7 @@ int main(){
     int n = 10;
     for(int i = 1; i<=n; i++){
         if(i == 6){
-            continue;
+            continue;  // continue statement usin one time break the position....
         }
         printf("%d\n", i);
     }
