3f22a0c9b25d9ac837ddba435b7191de10f5efea
Md Naim <naim.cse2004@gmail.com>
2025-12-31 18:40:03 +0600

    add

--- CS/Introduction to Programming/Problem solving Conditional statement Module-5/find_digit.c
+++ CS/Introduction to Programming/Problem solving Conditional statement Module-5/find_digit.c
@@ -2,7 +2,7 @@
 int main(){
     int n;
     scanf("%d", &n);
-    int num = n%10;
+    int num = n%10; // %  ---> mainly use kora hoy kono number find korer jonne
     printf("%d", num);
     return 0;
 }
