8f2aec671062ef79ab51aa0ec3011e1c8831f9a0
Md Naim <naim.cse2004@gmail.com>
2025-12-31 20:27:55 +0600

    loop

--- CS/Introduction to Programming/Loop module-3/loop_with_condition_odd_even.c
+++ CS/Introduction to Programming/Loop module-3/loop_with_condition_odd_even.c
@@ -1,9 +1,13 @@
+
+
+//find odd even
+
 #include<stdio.h>
 int main(){
     int n;
     n = 20;
 
-    for( int i=0; i<=n; i++){
+    for( int i=0; i<=n; i++){ // 
         if(i % 2 == 0){
             printf("%d -- EVEN Number\n", i);
         }else{
