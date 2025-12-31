d21decb10a0c0b41e8e00012cee3679bc944cca5
Md Naim <naim.cse2004@gmail.com>
2025-12-31 18:24:54 +0600

    define of the variable

--- CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/pratcis3.c
+++ CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/pratcis3.c
@@ -1,7 +1,7 @@
 
 #include<stdio.h>
 int main(){
-    int num1, num2;
+    int num1, num2; //Declare datatype
     scanf("%d %d", &num1, &num2); // user input
 
     printf("num2=%d\n num1 = %d\n", num2, num1);
