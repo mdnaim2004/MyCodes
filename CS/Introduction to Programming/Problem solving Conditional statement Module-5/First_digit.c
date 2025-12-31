665b90d46776f1eb01cdef385102c69fda6e2eb8
Md Naim <naim.cse2004@gmail.com>
2025-12-31 18:41:04 +0600

    add

--- CS/Introduction to Programming/Problem solving Conditional statement Module-5/First_digit.c
+++ CS/Introduction to Programming/Problem solving Conditional statement Module-5/First_digit.c
@@ -1,10 +1,14 @@
+
+
+
+// this problem calculate first digit
 #include<stdio.h>
 int main(){
     int X, num;
     scanf("%d", &X);
     num = X/1000;
 
-    if(num % 2 == 1){
+    if(num % 2 == 1){ // 
         printf("ODD\n");
     }
     else{
