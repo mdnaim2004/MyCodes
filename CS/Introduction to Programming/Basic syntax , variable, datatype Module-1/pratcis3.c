7678fbf983bc66e6d1c2e4608083961faddfd569
Md Naim <naim.cse2004@gmail.com>
2025-12-31 18:24:03 +0600

    add name of the coment

--- CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/pratcis3.c
+++ CS/Introduction to Programming/Basic syntax , variable, datatype Module-1/pratcis3.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main(){
     int num1, num2;
-    scanf("%d %d", &num1, &num2);
+    scanf("%d %d", &num1, &num2); // user input
 
     printf("num2=%d\n num1 = %d\n", num2, num1);
 }
