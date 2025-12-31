8f9222f9b37901e65db2c52c8c3633a66e894fca
Md Naim <naim.cse2004@gmail.com>
2025-12-31 20:30:14 +0600

    New code

--- CS/Introduction to Programming/Loop module-3/count_1_to_.c
+++ CS/Introduction to Programming/Loop module-3/count_1_to_.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    for(int i=0; i<=100; i++){
+    for(int i=0; i<=100; i++){ //using for loop count 1 to n number 
         printf("%d\n", i);
     }
     return 0;
