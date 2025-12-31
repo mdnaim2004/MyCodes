31749c745c4d283bec6d17c6f53561762ed5b907
Md Naim <naim.cse2004@gmail.com>
2025-12-31 19:54:18 +0600

    Odd Even

--- CS/Introduction to Programming/Loop module-3/odd_even.c
+++ CS/Introduction to Programming/Loop module-3/odd_even.c
@@ -1,8 +1,11 @@
+
+
+//find odd even
 #include<stdio.h>
 int main(){
     int n = 14;
 
-    if(n % 2 == 0){
+    if(n % 2 == 0){ 
         printf("EVEN number");
     }else{
         printf("ODD number");
