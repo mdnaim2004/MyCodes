9ee4ec1544852fb74c8dcffc3359816ebd9d6621
Md Naim <naim.cse2004@gmail.com>
2026-01-10 01:10:06 +0600

    stl list

--- CS/Data Structure/STL List and Cycle Detection M-9/insert_at_any_popsition.cpp
+++ CS/Data Structure/STL List and Cycle Detection M-9/insert_at_any_popsition.cpp
@@ -1,13 +1,28 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     list<int> l ={10, 20 ,30};
+//     l.insert(next(l.begin(),2), 100);
+
+//     for(int val : l){
+//         cout << val << " ";
+//     }
+    
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    list<int> l ={10, 20 ,30};
-    l.insert(next(l.begin(),2), 100);
+    list<int> l = {10,20,30,40,50};
+    list<int> l2 = {100,200,300,400};
 
+    l.insert(next(l.begin(), 2), l2.begin(), l2.end());
     for(int val : l){
         cout << val << " ";
     }
-    
     return 0;
 }
