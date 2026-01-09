0163a4058ac49f5bce109921fd635f4066ebecbf
Md Naim <naim.cse2004@gmail.com>
2026-01-09 22:12:13 +0600

    add new code for linklist

---
+++ CS/Data Structure/STL List and Cycle Detection M-9/constructor_stl_list.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    //     list<int> mylist   timecomplexiy = O(1)
+
+    // same as a vector
+
+    // list<int> mylist;
+    // cout<<mylist.size() << endl;
+
+    return 0;
+}
