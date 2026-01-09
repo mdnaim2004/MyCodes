4fac7c47bd2126e619e53a7d963523394c76bc5b
Md Naim <naim.cse2004@gmail.com>
2026-01-10 00:32:25 +0600

    stl list

---
+++ CS/Data Structure/STL List and Cycle Detection M-9/capacity_stl_list.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list<int> l = {10,20,30,40,50};
+
+    // l.clear();
+
+    // l.clear();
+
+    cout <<"list size = "<< l.size() << endl;
+
+
+
+    // for(int val : l){
+    //     cout << val << " ";
+    // }
+
+    if(l.empty()){
+        cout <<"Empty";
+    }
+    else{
+        cout << "Not Empty";
+    }
+
+    return 0;
+}
