ea6f794aaad1f478dc5e28a5bdda16f228981bcf
Md Naim <naim.cse2004@gmail.com>
2026-01-10 00:52:13 +0600

    pop_back() and pop_front()

--- CS/Data Structure/STL List and Cycle Detection M-9/Modifire_STL_list.cpp
+++ CS/Data Structure/STL List and Cycle Detection M-9/Modifire_STL_list.cpp
@@ -7,10 +7,21 @@ int main() {
     list<int> l2;
     l2 =l;
 
+
+    // pichone value add kora
     l2.push_back(100);
+
+    // samne value add kora
     l2.push_front(500);
 
-    
+    //pichone theke value bad deoa
+    l2.pop_back();
+
+    //samne theke value bad deoa
+    l2.pop_front();
+
+    l2.pop_back();
+    l2.pop_front();
 
     for(int val : l2){
         cout << val << " ";
