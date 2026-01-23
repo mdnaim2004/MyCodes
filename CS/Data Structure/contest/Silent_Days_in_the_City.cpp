0fd083211ce96c0c5cb643d98e10fd8a11300785
Md Naim <naim.cse2004@gmail.com>
2026-01-23 23:10:42 +0600

    unsolve this problem

---
+++ CS/Data Structure/contest/Silent_Days_in_the_City.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+
+    vector<int> arr(n);
+
+    for(int i=1; i<=n; i++){
+        cin >> arr[i];
+    }
+    int q;
+    cin >> q;
+
+    int l, r;
+
+    for(int i=0; i<q; i++){
+        cin >> l >> r;
+    }
+
+
+    
+
+    // for(int i=1; i<=n; i++){
+    //     cout << arr[i] << " ";
+    // }
+    
+    // cout << endl;
+
+    // cout << q << endl;
+
+    // for(int i=0; i<q; i++){
+    //     cout << l << " " << r << endl;
+    // }
+}
