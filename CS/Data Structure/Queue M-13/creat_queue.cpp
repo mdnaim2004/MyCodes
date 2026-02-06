c3242be352056cf767527dd25062907ba0338975
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-06 11:40:28 +0600

    implement Queue using singli linklist

---
+++ CS/Data Structure/Queue M-13/creat_queue.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        
+    }
+};
+class myqueue{
+    public:
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int siz = 0;
+
+    void push(int val){
+
+        siz++;
+
+        Node* newnode = new Node(val);
+
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+
+        tail->next = newnode;
+        tail = newnode;
+    }
+
+    void pop(){
+
+        siz--;
+
+        Node* deletnode = head;
+
+        head = head->next;
+        delete deletnode;
+        
+        if(head == NULL){
+            tail = NULL;
+        }
+    }
+
+    int front(){
+        return head->val;
+    }
+
+    int back(){
+        return tail->val;
+    }
+
+    int size(){
+        return siz;
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+};
+int main(){
+
+    // somthing here...............................................
+    
+    return 0;
+}
