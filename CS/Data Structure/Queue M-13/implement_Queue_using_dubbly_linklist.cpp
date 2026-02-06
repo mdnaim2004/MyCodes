f90eb892c7b3dc5eb508fbf8c2e3c41faf3ea466
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-06 19:06:46 +0600

    implement queue using dubbly linklist

---
+++ CS/Data Structure/Queue M-13/implement_Queue_using_dubbly_linklist.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+class myqueue{
+    public:
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int siz = 0;
+
+    void push(int val){
+        siz++;
+        Node* newnode = new Node(val);
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        tail->next = newnode;
+        newnode->prev = tail;
+        tail = newnode;    
+    }
+    void pop(){
+        siz--;
+        Node* deletnode = head;
+
+        head = head->next;
+        delete deletnode;
+
+        if(head == NULL){
+            tail = NULL;
+            return;
+        }
+        head->prev = NULL;
+    }
+
+    int front(){
+        return head->val;
+    }
+
+    int back(){
+        return tail->val;
+    }
+
+    int size(){
+        return siz;
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+
+};
+
+int main(){
+
+    myqueue q;
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int val ;
+        cin >> val;
+        q.push(val);
+    }
+    cout << "front val =  "<<  q.front() << ";"<<" back val = " << q.back()<< ";" << " queue size = "  << q.size() << " " << endl;
+
+    while(!q.empty()){
+        cout<< q.front() << endl;
+        q.pop();
+    }
+    
+    return 0;
+}
