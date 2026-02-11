dec31ac6de65de37d28cc4daeb4fa3637338485e
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-12 00:10:33 +0600

    learn input binary tree properly

---
+++ 6500/BST/input_binary_tree.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_binary_tree(){
+    int val;
+    cin >> val;
+
+    Node* root;
+    if(val == -1) root = NULL;
+    else root = new Node(val);
+
+    queue<Node*> q;
+
+    if(root) q.push(root);
+
+    while(!q.empty()){
+        Node* par = q.front();
+        q.pop();
+
+        int l,r;
+        cin >> l >> r;
+
+        Node* myleft, *myright;
+
+        if(l == -1) myleft = NULL;
+        else myleft = new Node(l);
+
+        if(r == -1) myright = NULL;
+        else myright = new Node(r);
+
+        par->left = myleft;
+        par->right = myright;
+
+        if(par->left) q.push(par->left);
+        if(par->right) q.push(par->right);
+    }
+    return root;
+}
+
+int main(){
+    
+    return 0;
+}
