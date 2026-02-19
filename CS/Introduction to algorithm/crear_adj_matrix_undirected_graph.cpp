e297e5fd3900f4600587345ca6320c9246e45117
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-19 22:18:48 +0600

    input value of adjancy matrics

--- CS/Introduction to algorithm/crear_adj_matrix_undirected_graph.cpp
+++ CS/Introduction to algorithm/crear_adj_matrix_undirected_graph.cpp
@@ -15,12 +15,20 @@ int main(){
     //shortcut form.......
     memset(adj_mat, 0, sizeof(adj_mat));
 
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<n; j++){
-    //         //adj_mat[i][j] = 0;
-    //         cout << adj_mat[i][j] <<" ";
-    //     }
-    //     cout << endl;
-    // }
+    for(int i=0; i<e; i++){
+        int a,b;
+        cin >> a >> b;
+
+        adj_mat[a][b] = 1;
+        adj_mat[b][a] = 1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            //adj_mat[i][j] = 0;
+            cout << adj_mat[i][j] <<" ";
+        }
+        cout << endl;
+    }
     return 0;
 }
