39e5e16ff8617d1ba0aef38dffc90acd458dce04
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-19 22:20:47 +0600

    hander corner case of the times of corner value ifs 1

--- CS/Introduction to algorithm/crear_adj_matrix_undirected_graph.cpp
+++ CS/Introduction to algorithm/crear_adj_matrix_undirected_graph.cpp
@@ -14,6 +14,13 @@ int main(){
 
     //shortcut form.......
     memset(adj_mat, 0, sizeof(adj_mat));
+    
+    
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            if(i==j)
+                adj_mat[i][j]=1;
+
 
     for(int i=0; i<e; i++){
         int a,b;
