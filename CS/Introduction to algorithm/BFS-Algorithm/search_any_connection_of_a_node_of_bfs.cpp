ab3029583f3f2c124467d0c1707b7cf59390c212
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-21 16:00:40 +0600

    new problem solve

--- CS/Introduction to algorithm/BFS-Algorithm/search_any_connection_of_a_node_of_bfs.cpp
+++ CS/Introduction to algorithm/BFS-Algorithm/search_any_connection_of_a_node_of_bfs.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> adj_list[1005];
-bool vis[1004];
+vector<int> adj_list[1005]; //global variable declare
+bool vis[1004]; // global variable declare......
 
 void bfs(int src){
-    queue<int> q;
+    queue<int> q; // 
     q.push(src);
 
-    vis[src] = true;
+    vis[src] = true; //all  visited value ar false to true.......step by step
 
-    while(!q.empty()){
-        int par = q.front();
-        q.pop();
-        for(int child : adj_list[par]){
-            if(!vis[child]){
-                q.push(child);
-                vis[child] = true;
+    while(!q.empty()){  // we starting a loop while queue is not empty...
+        int par = q.front(); // then queue er first value ke parent nam dilam
+        q.pop(); //then ak ak kore value gulo ber kore anlam
+        for(int child : adj_list[par]){  // akhene akta foreach loop use korlam jate amar vector er vitor je 2D array ase seitar vitor ki ki ase ta tavarse kore dekha jai....
+            if(!vis[child]){ // then amra akhene check kortechi, je akhene ami kon kon value gulo tavarse korchi.....
+                q.push(child); // jodi tavarse na kore thaki tahole loop er vitor dhuke ami queue er vitor amar jei value ta nilam seita push korbo
+                vis[child] = true; // then ami akhene amar jei visited array ta chilo seita true kore dilam
             }
         }
     }
@@ -39,6 +39,6 @@ int main(){
 
     bfs(src);
 
-    if(vis[dest]) cout << "YES\n";else cout << "NO\n";
+    if(vis[dest]) cout << "YES\n";else cout << "NO\n"; // akhene ami check kortechi je amar condition ta true or false
     return 0;
 }
