444bf2714da8e1e853270d7aba57901ba5d1b452
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-25 17:12:46 +0600

    2D grid on the matrix of the DFS print nighbour

--- CS/Introduction to algorithm/DFS-Algorithm/dfs_on_2D_grid.cpp
+++ CS/Introduction to algorithm/DFS-Algorithm/dfs_on_2D_grid.cpp
@@ -4,7 +4,20 @@ using namespace std;
 char grid[105][105];
 bool vis[105][105];
 
+vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
 
+void dfs(int si,int sj){
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+
+    for(int i=0; i<4; i++){
+        int ci,cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+
+        cout << ci << " "  << cj << endl;
+    }
+}
 
 int main(){
     int n,m;
@@ -14,12 +27,9 @@ int main(){
         for(int j=0; j<m; j++)
             cin >> grid[i][j];
 
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<m; j++){
-    //         cout << grid[i][j];
-    //     }
-    //     cout << endl; 
-    // }
-
+    int si,sj;
+    cin >> si >> sj;
+    dfs(si,sj);
+    
     return 0;
 }
