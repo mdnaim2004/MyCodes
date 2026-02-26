4efa378dac81e5ea74fa6cf841934bdfbfbdd34a
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-26 15:48:34 +0600

    here is 2d gread problem on this depth first search

--- CS/Introduction to algorithm/DFS-Algorithm/dfs_on_2D_grid.cpp
+++ CS/Introduction to algorithm/DFS-Algorithm/dfs_on_2D_grid.cpp
@@ -4,7 +4,15 @@ using namespace std;
 char grid[105][105];
 bool vis[105][105];
 
+int n,m;
+
 vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};
+
+bool valid(int i, int j){
+    if(i<0 || i>=n || j<0 || j>=m)
+        return false;
+    return true;
+}
 
 void dfs(int si,int sj){
     cout << si << " " << sj << endl;
@@ -15,12 +23,16 @@ void dfs(int si,int sj){
         ci = si + d[i].first;
         cj = sj + d[i].second;
 
-        cout << ci << " "  << cj << endl;
+        //cout << ci << " "  << cj << endl;
+
+        if(valid(ci,cj) && !vis[ci][cj])
+            dfs(ci,cj);
     }
 }
 
+
 int main(){
-    int n,m;
+    
     cin >> n >> m;
 
     for(int i=0; i<n; i++)
@@ -29,6 +41,8 @@ int main(){
 
     int si,sj;
     cin >> si >> sj;
+
+    memset(vis,false,sizeof(vis));
     dfs(si,sj);
     
     return 0;
