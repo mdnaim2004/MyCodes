bc611a806aa72020f6ccf60f9e6cbb5e9db6fc0f
mdnaim2004 <naim.cse2004@gmail.com>
2026-02-27 11:09:37 +0600

    solving another new problem of 2D gread, of the count room on the grid problem  .....

---
+++ CS/Introduction to algorithm/Exam/Count_Apartments_II.cpp
@@ -0,0 +1,68 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+char grid[1005][1005];
+bool vis[1005][1005];
+int level[1005][1005];
+
+vector<pair<int, int>> d = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+int m,n;
+
+bool valid(int i, int j){
+    if(i<0 || i >= n || j<0 || j >= m) return false;
+    else return true;
+}
+
+void bfs(int si,int sj){
+    queue<pair<int,int>> q;
+    q.push({si,sj});
+    vis[si][sj] = true;
+
+    level[si][sj] = true;
+
+    while(!q.empty()){
+        pair<int,int> par = q.front();
+        q.pop();
+
+        int par_i = par.first;
+        int par_j = par.second;
+
+        for(int i=0; i<4; i++){
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+
+            if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.'){
+                q.push({ci, cj});
+                vis[ci][cj] = true;       
+                level[ci][cj] = level[par_i][par_j] + 1;   
+            }
+        }
+    }
+}
+
+
+int main(){
+    //int n, m;
+    cin >> n >>m;
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin >> grid[i][j];
+
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+
+    //int count = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(grid[i][j] == '.' && !vis[i][j]){
+                // count++;
+                cout << level[i][j] << endl;
+                bfs(i, j);
+            }
+        }
+    }
+    //cout << count <<endl;
+    return 0;
+}
