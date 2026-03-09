35ddb2128c51103a8d835bced7d582ccdbeb2075
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-09 23:56:35 +0600

    Simplify DFS cycle detection logic by removing unnecessary line breaks and consolidating conditionals

--- CS/Introduction to algorithm/cycle detection/directed_graph_cycle_detection_DFS.cpp
+++ CS/Introduction to algorithm/cycle detection/directed_graph_cycle_detection_DFS.cpp
@@ -4,24 +4,16 @@ using namespace std;
 vector<int> adj_list[100005];
 bool vis[100005];
 bool pathvis[100005];
-
 bool cycle;
 
 void dfs(int src){
     vis[src] = true;
-
     pathvis[src] = true;
 
     for(int child : adj_list[src]){
 
-        if(vis[child] && pathvis[child]){
-            cycle = true;
-        }
-        if(vis[child] == false){
-            
-            dfs(child);
-            
-        }
+        if(vis[child] && pathvis[child]) cycle = true;
+        if(vis[child] == false)  dfs(child);     
     }
     pathvis[src] = false;
 }
@@ -43,9 +35,7 @@ int main(){
     cycle = false;
 
     for(int i=0; i<n; i++){
-        if(vis[i] == false){
-            dfs(i);
-        }
+        if(vis[i] == false) dfs(i);
     }
 
     if(cycle) cout << "Cycle detected"<< endl;
