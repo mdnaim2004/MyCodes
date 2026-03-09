7fb06e742b1b8de3be3d80d5f875b6a4d890d735
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-09 19:34:43 +0600

    Refactor DFS implementation for undirected graph cycle detection and add problem configuration file

--- CS/Introduction to algorithm/cycle detection/undirected_graph_cycleDected_using_DFS.cpp
+++ CS/Introduction to algorithm/cycle detection/undirected_graph_cycleDected_using_DFS.cpp
@@ -10,7 +10,10 @@ void dfs(int src){
     vis[src] = true;
 
     for(int child : adj_list[src]){
-        
+        if(vis[child] == false){
+            dfs(child);
+            parent[child] = src;
+        }
     }
 }
 
@@ -19,7 +22,24 @@ int main(){
     cin >> n >> e;
 
     while(e--){
+        int a, b;
+        cin >>a >> b;
 
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(false));
+    memset(parent,-1, sizeof(parent));
+
+    cycle = false;
+
+    for(int i=0; i<n; i++){
+        if(vis[i] == false){
+            dfs(i);
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout << i << " parent " << parent[i] << endl;
     }
     return 0;
 }
