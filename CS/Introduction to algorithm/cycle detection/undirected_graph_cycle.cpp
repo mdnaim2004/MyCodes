8a7a712731c7118bc273950983a3c46cf9a7bd5c
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-09 17:01:16 +0600

    Add implementation for undirected graph cycle detection using BFS

---
+++ CS/Introduction to algorithm/cycle detection/undirected_graph_cycle.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1000];
+bool vis[1000];
+
+void bfs(int src){
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+
+    while(!q.empty()){
+        int par = q.front();
+        q.pop();
+
+        for(int child : adj_list[par]){
+            if(vis[par] == false){
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
+int main(){
+    int n, e;
+    cin>> n >> e;
+    
+    while(e--){
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+
+    }
+
+    memset(vis, false, sizeof(vis));
+
+    for(int i=0; i<n; i++){
+        if(vis[i] == false){
+            bfs(i);
+        }
+    }
+    //memset()
+    return 0;
+}
