82970223f04525223391e509552b9098814cce9b
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-09 18:11:14 +0600

    Add initial implementation for undirected graph cycle detection using DFS

---
+++ CS/Introduction to algorithm/cycle detection/undirected_graph_cycleDected_using_DFS.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1000];
+bool vis[1000];
+int parent[1000];
+bool cycle;
+
+void dfs(int src){
+    vis[src] = true;
+
+    for(int child : adj_list[src]){
+        
+    }
+}
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+
+    while(e--){
+
+    }
+    return 0;
+}
