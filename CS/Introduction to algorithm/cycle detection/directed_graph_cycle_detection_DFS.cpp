97791caaa9871a4505ffa07dace44fd04173d597
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-09 22:42:37 +0600

    Add directed graph cycle detection implementation using DFS

---
+++ CS/Introduction to algorithm/cycle detection/directed_graph_cycle_detection_DFS.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[100005];
+bool vis[100005];
+int parent[100005];
+bool cycle;
+
+void dfs(int src){
+    vis[src] = true;
+
+    for(int child : adj_list[src]){
+        if(vis[child] && parent[src] != child){
+            cycle = true;
+        }
+        if(vis[child] == false){
+            parent[child] = src;  //this are actually processs....
+            dfs(child);
+            //parent[child] = src;
+        }
+    }
+}
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+
+    while(e--){
+        int a, b;
+        cin >> a >> b;
+
+        adj_list[a].push_back(b);
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(parent, -1, sizeof(parent));
+
+    cycle = false;
+
+    for(int i=0; i<n; i++){
+        if(vis[i] == false){
+            dfs(i);
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        cout << i << " parent " << parent[i] << endl;
+    }
+
+    if(cycle) cout << "Cycle detected"<< endl;
+    else cout << "No Cycle" << endl;
+
+    return 0;
+}
