e47e7d7d4392b860a649c75a2fcc9ae65a72fc5c
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-12 00:25:24 -0700

    dijkstra algorithm

--- CS/Introduction to algorithm/Dijkstra Algorithm/single_shortest_path.cpp
+++ CS/Introduction to algorithm/Dijkstra Algorithm/single_shortest_path.cpp
@@ -1,16 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<pair<int,int>> adj_list[1000]; // Assuming maximum 1000 nodes
+int dis[1000];
 
+void dijkstra(int src){
+    queue<pair<int,int>> q;
+    q.push({src,0});
+    dis[src] = 0;
+
+    while(!q.empty()){
+        pair<int,int> par = q.front();
+        q.pop();
+
+        int par_node = par.first;
+        int par_dis = par.second;
+
+        for(auto child : adj_list[par_node]){
+            int child_node = child.first;
+            int child_dis = child.second;
+
+            if(par_dis + child_dis < dis[child_node] ){
+                dis[child_node] = par_dis + child_dis;
+                q.push({child_node, dis[child_node]});
+            }
+        }
+    }
+}
 int main(){
     int n, e;
-    cin >> n >>e;
+    cin >> n >>e; //inpt the number of the node and edge
 
-    vector<pair<int,int>> adj_list[n];
+    
     while(e--){
         int a, b, c;
         cin >> a >> b >> c;
-        adj_list[a].push_back({b,c});
+        adj_list[a].push_back({b,c});   
         adj_list[b].push_back({a,c});
     }
     for(int i=0; i<n; i++){
