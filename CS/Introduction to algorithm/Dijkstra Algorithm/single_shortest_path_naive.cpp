3aa495257e891fc45d9445546d6264dc70b1743c
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-13 16:42:51 +0600

    add Dijkstra algorithm implementation

---
+++ CS/Introduction to algorithm/Dijkstra Algorithm/single_shortest_path_naive.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> adj_list[1000]; // Assuming maximum 1000 nodes
+int dis[1000];
+
+void dijkstra(int src){
+    queue<pair<int,int>> q;
+    q.push({src,0});
+    dis[src] = 0;
+
+    while(!q.empty()){
+        pair<int,int> par = q.front();
+        q.pop();
+
+        int par_node = par.first;
+        int par_dis = par.second;
+
+        for(auto child : adj_list[par_node]){
+            int child_node = child.first;
+            int child_dis = child.second;
+
+            if(par_dis + child_dis < dis[child_node] ){
+                dis[child_node] = par_dis + child_dis;
+                q.push({child_node, dis[child_node]});
+            }
+        }
+    }
+}
+int main(){
+    int n, e;
+    cin >> n >>e; //inpt the number of the node and edge
+
+    
+    while(e--){                                  /* 5 8
+                                                    0 1 10
+                                                    1 2 1
+                                                    0 2 7
+                                                    0 3 4
+                                                    2 3 1
+                                                    3 4 5
+                                                    1 4 3
+                                                    2 4 5*/
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_list[a].push_back({b,c});   
+        adj_list[b].push_back({a,c});
+    }
+    for(int i=0; i<n; i++){
+        cout<< i << " => ";
+        for(pair<int,int> p : adj_list[i]){
+            cout <<i<<","<< p.first << " -> " << p.second << ", ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
