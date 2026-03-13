4040a644bc836181434dafaba8cc9da7d6883bb8
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-13 17:06:26 +0600

    refactor Dijkstra algorithm implementation for clarity and efficiency; add test configuration

--- CS/Introduction to algorithm/Dijkstra Algorithm/single_shortest_path_naive.cpp
+++ CS/Introduction to algorithm/Dijkstra Algorithm/single_shortest_path_naive.cpp
@@ -1,60 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int,int>> adj_list[1000]; // Assuming maximum 1000 nodes
-int dis[1000];
+const int N = 1000;
+const int INF = INT_MAX;
 
-void dijkstra(int src){
-    queue<pair<int,int>> q;
-    q.push({src,0});
+vector<pair<int, int>> adj_list[N];
+int dis[N];
+
+void dijkstra(int src) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
     dis[src] = 0;
+    pq.push({0, src}); // {distance, node}
 
-    while(!q.empty()){
-        pair<int,int> par = q.front();
-        q.pop();
+    while (!pq.empty()) {
+        pair<int, int> par = pq.top();
+        pq.pop();
 
-        int par_node = par.first;
-        int par_dis = par.second;
+        int par_dis = par.first;
+        int par_node = par.second;
 
-        for(auto child : adj_list[par_node]){
+        // Ignore outdated entries
+        if (par_dis > dis[par_node]) continue;
+
+        for (auto child : adj_list[par_node]) {
             int child_node = child.first;
             int child_dis = child.second;
 
-            if(par_dis + child_dis < dis[child_node] ){
+            if (par_dis + child_dis < dis[child_node]) {
                 dis[child_node] = par_dis + child_dis;
-                q.push({child_node, dis[child_node]});
+                pq.push({dis[child_node], child_node});
             }
         }
     }
 }
-int main(){
-    int n, e;
-    cin >> n >>e; //inpt the number of the node and edge
 
-    
-    while(e--){                                  /* 5 8
-                                                    0 1 10
-                                                    1 2 1
-                                                    0 2 7
-                                                    0 3 4
-                                                    2 3 1
-                                                    3 4 5
-                                                    1 4 3
-                                                    2 4 5*/
+int main() {
+    int n, e;
+    cin >> n >> e;
+
+    while (e--) {
         int a, b, c;
         cin >> a >> b >> c;
-        adj_list[a].push_back({b,c});   
-        adj_list[b].push_back({a,c});
+        adj_list[a].push_back({b, c});
+        adj_list[b].push_back({a, c}); // remove this if graph is directed
     }
-    for(int i=0; i<n; i++){
-        cout<< i << " => ";
-        for(pair<int,int> p : adj_list[i]){
-            //cout <<i<<","<< p.first << " -> " << p.second << ", ";
-            cout << i << "-> " << dis[i] << endl;
-        }
+
+    for (int i = 0; i < n; i++) {
+        dis[i] = INF;
+    }
+
+    int src;
+    cin >> src;
+
+    dijkstra(src);
+
+    for (int i = 0; i < n; i++) {
+        cout << i << " -> ";
+        if (dis[i] == INF)
+            cout << "INF";
+        else
+            cout << dis[i];
         cout << endl;
     }
+
     return 0;
 }
 
-
+/*
+5 8
+0 1 10
+1 2 1
+0 2 7
+0 3 4
+2 3 1
+3 4 5
+1 4 3
+2 4 5
+*/
