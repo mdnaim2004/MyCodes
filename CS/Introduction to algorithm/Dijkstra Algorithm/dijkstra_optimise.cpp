ee823670c1ed64f5477318ee8b64db9e19ce6689
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-13 19:48:54 +0600

    refactor dijkstra function to initialize distance array and streamline logic

--- CS/Introduction to algorithm/Dijkstra Algorithm/dijkstra_optimise.cpp
+++ CS/Introduction to algorithm/Dijkstra Algorithm/dijkstra_optimise.cpp
@@ -8,6 +8,26 @@ void dijkstra(int src){
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     pq.push({0, src});
 
+    dis[src] = 0;
+
+    while(!pq.empty()){
+        pair<int,int> par = pq.top();
+        pq.pop();
+
+        int par_node = par.second;
+        int par_dis = par.first;
+
+        for(auto child : adj_list[par_node]){
+            int child_node = child.first;
+            int child_dis = child.second;
+
+            if(par_dis + child_dis < dis[child_node]){
+                dis[child_node] = par_dis + child_dis;
+                pq.push({dis[child_node], child_node});
+            }
+        }
+    }
+
 }
 
 int main(){
