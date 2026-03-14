490ce3ff6fa9bcd7643709ee0da83babdafd5e4f
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-14 12:20:14 +0600

    feat: add Knight Moves solution implementation for HackerRank exam

---
+++ CS/Introduction to algorithm/Assignment/Knight_Moves.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char grid[1005][1005];
+bool vis[1005][1005];
+vector<pair<int, int>> d = {{-1, 0}, {1,0}, {0,-1}, {0, 1}};
+
+int n,m;
+bool valid(int i, int j){
+    if(i < 0 || i >= n || j< 0 || j>=m) 
+        return false;
+    return true;
+}
+
+void dfs(int si, int sj){
+    vis[si][sj] = true;
+    for(int i=0; i<4; i++){
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
+            dfs(ci, cj);
+    }
+}
+int main(){
+    
+
+    int si,sj, di,dj;
+    cin >> si >> sj;
+    cin >> di >> dj;
+
+    memset(vis, false, sizeof(vis));
+
+    if(grid[si][sj] == '-' || grid[di][dj] == '-'){
+        cout << "NO" << endl;
+        return 0;
+    }
+    dfs(si, sj);
+
+    if(vis[di][dj] == true) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
