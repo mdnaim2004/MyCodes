d43d0c67c5a5de4dcf31e8af1d6dcfe96914a688
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-14 22:22:42 +0600

    refactor Knight Moves solution: replace DFS with BFS for pathfinding and improve grid handling

--- CS/Introduction to algorithm/Assignment/Knight_Moves.cpp
+++ CS/Introduction to algorithm/Assignment/Knight_Moves.cpp
@@ -1,43 +1,69 @@
 #include<bits/stdc++.h>
 using namespace std;
+char grid[105][105];
+bool vis[105][105];
+int level[105][105];
 
-char grid[1005][1005];
-bool vis[1005][1005];
-vector<pair<int, int>> d = {{-1, 0}, {1,0}, {0,-1}, {0, 1}};
-
+vector<pair<int,int>> d = {{-1,2},{1,2},{1,-2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
 int n,m;
+
 bool valid(int i, int j){
-    if(i < 0 || i >= n || j< 0 || j>=m) 
+    if(i<0 || i>=n || j<0 || j>=m)
         return false;
     return true;
 }
 
-void dfs(int si, int sj){
+void bfs(int si, int sj){
+    queue<pair<int,int>> q;
+    q.push({si,sj});
+
     vis[si][sj] = true;
-    for(int i=0; i<4; i++){
-        int ci = si + d[i].first;
-        int cj = sj + d[i].second;
-        if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
-            dfs(ci, cj);
+    level[si][sj] = 0;
+
+    while(!q.empty()){
+        pair<int ,int> par = q.front();
+        q.pop();
+
+        int par_i = par.first;
+        int par_j = par.second;
+
+        //cout << par_i << " " << par_j << endl;
+
+        for(int i=0; i<8; i++){
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+
+            if(valid(ci,cj) && !vis[ci][cj]){
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[par_i][par_j] + 1;
+            }
+        }
     }
 }
+
+
+
 int main(){
+    // int n,m;
     
+    int t;
+    cin >> t;
 
-    int si,sj, di,dj;
-    cin >> si >> sj;
-    cin >> di >> dj;
+    while(t--){
+        cin >> n >> m;
 
-    memset(vis, false, sizeof(vis));
+        int si, sj, di, dj;
+        cin >> si >> sj;
+        cin >> di >> dj;
 
-    if(grid[si][sj] == '-' || grid[di][dj] == '-'){
-        cout << "NO" << endl;
-        return 0;
+        memset(vis,false,sizeof(vis));
+        memset(level, -1, sizeof(level));
+
+        bfs(si, sj);
+
+        cout << level[di][dj] << endl;
     }
-    dfs(si, sj);
-
-    if(vis[di][dj] == true) cout << "YES" << endl;
-    else cout << "NO" << endl;
 
     return 0;
 }
