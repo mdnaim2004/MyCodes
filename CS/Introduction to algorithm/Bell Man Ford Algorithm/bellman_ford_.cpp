18509ce9760099637e9e04f241cb68dae4c1d37d
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-17 01:36:32 +0600

    style: format code for improved readability and consistency

--- CS/Introduction to algorithm/Bell Man Ford Algorithm/bellman_ford_.cpp
+++ CS/Introduction to algorithm/Bell Man Ford Algorithm/bellman_ford_.cpp
@@ -1,10 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Edge{
-    public:
-    int a,b, c;
-    Edge(int a, int b, int c){
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
         this->a = a;
         this->b = b;
         this->c = c;
@@ -13,43 +15,72 @@ class Edge{
 
 int dis[1005];
 vector<Edge> edge_list;
-int n , e;
+int n, e;
 
-void bellman_ford(){
-    for(int i=0; i<n-1; i++){
-        for(auto ed : edge_list){
+void bellman_ford()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
 
-            int a,b,c;
+            int a, b, c;
 
             a = ed.a;
             b = ed.b;
             c = ed.c;
 
-            if(dis[a] != INT_MAX && dis[a] + c < dis[b]){
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            {
                 dis[b] = dis[a] + c;
             }
         }
     }
+    bool cycle = false;
+
+    for (auto ed : edge_list){
+        int a, b, c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b]){
+            //dis[b] = dis[a] + c;
+            cycle = true;
+        }
+    }
+    if(cycle)
+        cout<<"Negitive weighted cycle detected."<<endl;
+    else{
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << " => " << dis[i] << endl;
+        }
+    }
+
 }
 
-int main(){
+int main()
+{
     cin >> n >> e;
-    while(e--){
+    while (e--)
+    {
         int a, b, c;
         cin >> a >> b >> c;
         edge_list.push_back(Edge(a, b, c));
     }
 
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         dis[i] = INT_MAX;
     }
     dis[0] = 0;
 
     bellman_ford();
 
-    for(int i=0; i<n; i++){
-        cout << i << " => " << dis[i] << endl;
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << i << " => " << dis[i] << endl;
+    // }
 
     return 0;
 }
