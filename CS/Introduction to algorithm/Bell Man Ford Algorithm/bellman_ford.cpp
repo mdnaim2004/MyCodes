04d3c955f0d7f083e867d7399698580843d7cc31
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-16 19:27:59 +0600

    feat: implement initial structure for Bellman-Ford algorithm

---
+++ CS/Introduction to algorithm/Bell Man Ford Algorithm/bellman_ford.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, e;
+    cin >> n >> e;
+    while(e--){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        
+    }
+    return 0;
+}
