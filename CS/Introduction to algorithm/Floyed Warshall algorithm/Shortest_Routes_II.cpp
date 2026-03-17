ec985e55d1fca73fe21a10c593ce465603bbfa06
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-17 23:32:13 +0600

    fix: correct loop boundaries in adjacency matrix initialization for Floyd-Warshall algorithm

--- CS/Introduction to algorithm/Floyed Warshall algorithm/Shortest_Routes_II.cpp
+++ CS/Introduction to algorithm/Floyed Warshall algorithm/Shortest_Routes_II.cpp
@@ -6,8 +6,8 @@ int main(){
     cin >> n>> m>> q;
     long long int adj_mat[n+5][n+5];
 
-    for(int i=1; i<n; i++){
-        for(int j=1; j<n; j++){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
             if(i == j)
                 adj_mat[i][j] = 0;
             else
@@ -23,9 +23,9 @@ int main(){
         adj_mat[b][a] = c;
     }
 
-    for(int k=1; k<n; k++){
-        for(int i=1; i<n; i++){
-            for(int j=1; j<n; j++){ 
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){ 
                 //cout << i << " " << k << " " << j << endl;
 
                 if(adj_mat[i][k] != LLONG_MAX && adj_mat[k][j] != LLONG_MAX && adj_mat[i][k] + adj_mat[j][k] < adj_mat[i][j])
