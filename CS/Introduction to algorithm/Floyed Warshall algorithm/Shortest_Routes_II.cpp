90da92182550f938ed23743005ea7438c6b378ed
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-17 23:31:06 +0600

    fix: update adjacency matrix type and initialization for Floyd-Warshall algorithm

--- CS/Introduction to algorithm/Floyed Warshall algorithm/Shortest_Routes_II.cpp
+++ CS/Introduction to algorithm/Floyed Warshall algorithm/Shortest_Routes_II.cpp
@@ -4,31 +4,31 @@ using namespace std;
 int main(){
     int n, m, q;
     cin >> n>> m>> q;
-    int adj_mat[n][n];
+    long long int adj_mat[n+5][n+5];
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    for(int i=1; i<n; i++){
+        for(int j=1; j<n; j++){
             if(i == j)
                 adj_mat[i][j] = 0;
             else
-                adj_mat[i][j] = INT_MAX;
+                adj_mat[i][j] = LLONG_MAX;
         }
     }
 
     while(m--){
-        int a, b, c;
+        long long int a, b, c;
         cin >> a>>b >> c;
 
         adj_mat[a][b] = c;
         adj_mat[b][a] = c;
     }
 
-    for(int k=0; k<n; k++){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){ 
+    for(int k=1; k<n; k++){
+        for(int i=1; i<n; i++){
+            for(int j=1; j<n; j++){ 
                 //cout << i << " " << k << " " << j << endl;
 
-                if(adj_mat[i][k] != INT_MAX && adj_mat[k][j] != INT_MAX && adj_mat[i][k] + adj_mat[j][k] < adj_mat[i][j])
+                if(adj_mat[i][k] != LLONG_MAX && adj_mat[k][j] != LLONG_MAX && adj_mat[i][k] + adj_mat[j][k] < adj_mat[i][j])
                     adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
             }
         }  
@@ -36,7 +36,7 @@ int main(){
     while(q--){
         int s, d;
         cin >> s >> d;
-        if(adj_mat[s][d] == INT_MAX) cout << -1 << endl;
+        if(adj_mat[s][d] == LLONG_MAX) cout << -1 << endl;
         else cout << adj_mat[s][d] << endl;
     }
 
