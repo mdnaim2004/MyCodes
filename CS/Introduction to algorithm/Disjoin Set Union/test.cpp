b6e0cfb1cc05ae0d3e89c1e683faf4459bf4be9b
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-19 10:18:51 +0600

    feat: add find function and main implementation for Disjoint Set Union

--- CS/Introduction to algorithm/Disjoin Set Union/test.cpp
+++ CS/Introduction to algorithm/Disjoin Set Union/test.cpp
@@ -1,8 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int main(){
-    cout << "test the envirnment are ok or not?";
-    cout << "All everything okey!";
-    return 0;
-}
+// tumi jeigulo bolle seigulo ki privious kono paper e chilo?
+// naki unique vabe amake diya dicho?
+// ar aro ki ki feature add kora jai ai code ja amar datagulo theke aro pure vabe paoa jai?
+// ar amar ai dataset ta future eo kaj e lagate chaschilam, to amar data diya cybercecurity ba , typing related, ba medically besoy e recharce kora kora jai, ai besoy e recharce ba thease kora jai, er kichu besoy thease kora jai ai besoy gulou mathai rakhbo,
+// mane amar daatse ke aro strong vabe korte chaschi , karon aivabe manush er kas theke data naoa valoi jhamelar,
+// to ami akbar ei valovabe data collect korte chaschilam.....
+// to jai hok amar 1st step holo stress er jonne kaj kora, 
+// to tai boltechi je amar dataset e ar ki ki besoy gulo add kora jai?jate amar dataset diya q1 ba q2 level e paper kora jai?
+// ar amar  dataset e input naor oi besoy ta te ektu jhamela ase, amar aki jinish bar bar asche....like seassion id, seasion 
+// aigulo....
+// to ami aigulo clear vabe chaschi,
+// ar amar code er onek kichu logical error ase, to seigulo thik korte hobbe, mane code er vitor valoi problem ase, jar karone amar dataset e onek vulval value asteche,
+// oi jinish gulou thik vabe handel korte hobbe.
