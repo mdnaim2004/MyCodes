861f744991a565715c6afbb7790bcb23b02ecf3b
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-19 15:09:13 +0600

    feat: enhance Disjoint Set Union with union function and group size initialization

--- CS/Introduction to algorithm/Disjoin Set Union/union.cpp
+++ CS/Introduction to algorithm/Disjoin Set Union/union.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int par[1005];
+int group_size[1005];
 
+//find operation....
 int find(int node){
     cout << node << endl;
     if(par[node] == -1){
@@ -14,15 +16,22 @@ int find(int node){
     return leader;
 }
 
+void dsu_union(int node1, int node2){
+
+    int leader1 = find(node1);
+    int leader2 = find(node2);
+
+    if(group_size[leader1] > group_size[leader2]){
+        par[leader2] = leader1;
+    }
+}
+
 int main(){
     memset(par, -1, sizeof(par));
+    memset(group_size, 1, sizeof(group_size));
 
-    par[0] = 1;
-    par[1] = -1;
-    par[2] = 1;
-    par[3] = 1;
-    par[4] = 5;
-    par[5] = 3;
+
+    dsu_union(1,2);
 
     cout << find(4) << endl;
     return 0;
