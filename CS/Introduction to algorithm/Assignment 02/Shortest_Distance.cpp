ba0f3c47a461d82a013a39abc660c334c9378b1e
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-20 06:34:06 +0600

    feat: remove unused input handling for edges and queries in Shortest Distance algorithm

--- CS/Introduction to algorithm/Assignment 02/Shortest_Distance.cpp
+++ CS/Introduction to algorithm/Assignment 02/Shortest_Distance.cpp
@@ -5,19 +5,6 @@ int main(){
     int n, e;
     cin >> n >> e;
 
-    while(e--){
-        int a,b,c;
-        cin >> a >> b >> c;
-    }
-
-
-    int q;
-    cin >>q;
-
-    while(q--){
-        int x, y;
-        cin >> x >> y;
-
-    }
+    
     return 0;
 }
