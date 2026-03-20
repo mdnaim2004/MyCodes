c80e866e6d3f1667107b6bbc379443b794bf0964
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-20 06:40:28 +0600

    feat: initialize adjacency matrix for Shortest Distance algorithm

--- CS/Introduction to algorithm/Assignment 02/Shortest_Distance.cpp
+++ CS/Introduction to algorithm/Assignment 02/Shortest_Distance.cpp
@@ -5,6 +5,22 @@ int main(){
     int n, e;
     cin >> n >> e;
 
-    
+    long long int adj_mat[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i == j)
+                adj_mat[i][j] = 0;
+            else
+                adj_mat[i][j] = LLONG_MAX;
+            
+        }
+    }
+
+    while(e--){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        adj_mat[a][b] = c;
+    }
     return 0;
 }
