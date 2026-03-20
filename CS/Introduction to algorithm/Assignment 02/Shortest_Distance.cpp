b9eb8a661d4b4ff31610106b357f559c3ef92447
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-20 06:33:20 +0600

    feat: add initial implementation of Shortest Distance algorithm

---
+++ CS/Introduction to algorithm/Assignment 02/Shortest_Distance.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, e;
+    cin >> n >> e;
+
+    while(e--){
+        int a,b,c;
+        cin >> a >> b >> c;
+    }
+
+
+    int q;
+    cin >>q;
+
+    while(q--){
+        int x, y;
+        cin >> x >> y;
+
+    }
+    return 0;
+}
