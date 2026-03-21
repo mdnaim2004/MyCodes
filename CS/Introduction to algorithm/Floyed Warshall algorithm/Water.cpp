cda985d1e65f8d7175ef29232247158fd60b3e1f
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-22 00:21:42 +0600

    feat: implement Cycle of Edges algorithm with disjoint set union and add Water problem solution

---
+++ CS/Introduction to algorithm/Floyed Warshall algorithm/Water.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int h[n];
+
+        for(int i=0; i<n; i++){
+            cin >> h[i];
+        }
+
+        int f_max = 0, s_max = 0;
+
+        for(int i=1; i<n; i++){
+            if(h[i] > h[f_max]){
+                f_max = i;
+                s_max = f_max;
+            }
+            else if(h[i] > h[s_max]){
+                s_max = i;
+            }
+        }
+        cout << f_max << " " << s_max << endl;
+    }
+    return 0;
+}
