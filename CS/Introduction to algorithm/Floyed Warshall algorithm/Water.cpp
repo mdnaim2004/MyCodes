d21dab32af6fb398bc734008870fbbf0df89b581
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-22 00:35:03 +0600

    fix: rename variables for clarity in Water problem solution

--- CS/Introduction to algorithm/Floyed Warshall algorithm/Water.cpp
+++ CS/Introduction to algorithm/Floyed Warshall algorithm/Water.cpp
@@ -7,27 +7,27 @@ int main(){
     while(t--){
         int n;
         cin >> n;
-        int h[n];
+        int high[n];
 
         for(int i=0; i<n; i++){
-            cin >> h[i];
+            cin >> high[i];
         }
 
-        int f_max = 0, s_max = -1;
+        int fst_max = 0, sec_max = -1;
 
         for(int i=1; i<n; i++){
-            if(h[i] > h[f_max]){
-                s_max = f_max;
-                f_max = i;
+            if(high[i] > high[fst_max]){
+                sec_max = fst_max;
+                fst_max = i;
             }
-            else if(h[i] > h[s_max]){
-                s_max = i;
+            else if(high[i] > high[sec_max]){
+                sec_max = i;
             }
         }
-        if(f_max >= s_max)
-            cout << s_max << " " << f_max << endl;
+        if(fst_max >= sec_max)
+            cout << sec_max << " " << fst_max << endl;
         else
-            cout << f_max << " " << s_max << endl;
+            cout << fst_max << " " << sec_max << endl;
 
     }
     return 0;
