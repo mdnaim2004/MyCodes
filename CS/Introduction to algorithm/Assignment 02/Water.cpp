197c1477dd05ce74c4ed5dd1fd668f680a53b674
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-22 12:01:25 +0600

    fix: implement Water problem solution with correct maximum index assignment

---
+++ CS/Introduction to algorithm/Assignment 02/Water.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int high[n];
+
+        for(int i=0; i<n; i++){
+            cin >> high[i];
+        }
+
+        int fst_max = 0, sec_max = -1;
+
+        for(int i=1; i<n; i++){
+            if(high[i] > high[fst_max]){
+                sec_max = fst_max;
+                fst_max = i;
+            }
+            else if(sec_max == -1 || high[i] > high[sec_max]){
+                sec_max = i;
+            }
+        }
+        if(fst_max >= sec_max)
+            cout << sec_max << " " << fst_max << endl;
+        else
+            cout << fst_max << " " << sec_max << endl;
+
+    }
+    return 0;
+}
