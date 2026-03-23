c3e2dde5e75eafcb5512c4a3bb0a070efd76e1ca
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-23 22:37:32 +0600

    feat: add memoization implementation for Fibonacci function

--- CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
+++ CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
@@ -1,22 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-int fibo(int n){
-    // if(n==0){
-    //     return 0;
-    // }
-    // if(n == 1){
-    //     return 1;
-    // }
+int fibo(int n){   // O(2^n)
 
-    // if(n==0 || n == 1){
-    //     return n;
-    // }
     if(n < 2){
         return n;
     }
     return fibo(n-1) + fibo(n-2);
 }
 int main(){
-    cout << fibo(9);
+    int n;
+    cin >> n;
+    cout << fibo(n);
     return 0;
 }
