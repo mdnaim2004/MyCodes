2cd49bea5f7121cdfac18649a979cc4d5501293c
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-23 18:14:42 +0600

    refactor: simplify base case handling in Fibonacci function

--- CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
+++ CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
@@ -8,7 +8,10 @@ int fibo(int n){
     //     return 1;
     // }
 
-    if(n==0 || n == 1){
+    // if(n==0 || n == 1){
+    //     return n;
+    // }
+    if(n < 2){
         return n;
     }
     return fibo(n-1) + fibo(n-2);
