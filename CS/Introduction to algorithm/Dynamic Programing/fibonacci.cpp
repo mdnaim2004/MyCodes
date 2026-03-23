8431ae1d2c9fb50c3c1da4c7e5acc211e62edd25
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-23 18:13:21 +0600

    optimise of the code of the fibonacci number of base case

--- CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
+++ CS/Introduction to algorithm/Dynamic Programing/fibonacci.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int fibo(int n){
-    if(n==0){
-        return 0;
-    }
-    if(n == 1){
-        return 1;
+    // if(n==0){
+    //     return 0;
+    // }
+    // if(n == 1){
+    //     return 1;
+    // }
+
+    if(n==0 || n == 1){
+        return n;
     }
     return fibo(n-1) + fibo(n-2);
 }
 int main(){
-    cout << fibo(5);
+    cout << fibo(9);
     return 0;
 }
