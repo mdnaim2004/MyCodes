0e0e0a2ef40981de8d2cc90b7bfe35f343be22f2
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-23 16:34:13 +0600

    feat: implement recursive function to print numbers from 1 to 5

--- CS/Introduction to algorithm/Dynamic Programing/test.cpp
+++ CS/Introduction to algorithm/Dynamic Programing/test.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void rec(int n){
+    if(n > 5)
+        return;
+    // cout << n << endl;
+    rec(n+1);
+    cout << n << endl;
+
+}
+
 int main(){
-    cout << "hello,World!" << endl;
+    rec(1);
+
     return 0;
 }
