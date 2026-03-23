5e72a7b8d9e764ede8624f7f9643e103bd387f37
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-24 00:18:18 +0600

    feat: refactor J. Factorial and D. Print Digits using Recursion implementations

--- CS/Introduction to algorithm/pratcis problem 14/D_Print_Digits_using_Recursion.cpp
+++ CS/Introduction to algorithm/pratcis problem 14/D_Print_Digits_using_Recursion.cpp
@@ -1,7 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+void rec(ll n){
+    if(n == 0)
+        return;
+    
+    rec(n/10);
+    cout << n%10 << " ";
+}
 
 int main(){
-    
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+
+        rec(n);
+        cout << endl;
+    }
     return 0;
 }
--- CS/Introduction to algorithm/pratcis problem 14/J_Factorial.cpp
+++ CS/Introduction to algorithm/pratcis problem 14/J_Factorial.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fact(int n){
-    if(n == 1){
+long long fact(int n){
+    if(n == 0 || n == 1){
         return 1;
     }
-    return n*fact(n-1);
+    return 1LL * n * fact(n - 1);
 }
 
 int main(){
     int n;
     cin >> n;
-    cout << fact(n) << endl;
+    cout << fact(n);
     return 0;
 }
