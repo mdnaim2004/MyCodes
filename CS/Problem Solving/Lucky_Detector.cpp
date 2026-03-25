ccf21c01b3c77ff43501d58c08210462de40cf73
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 21:19:08 +0600

    Add First & Last and Lucky Detector problem solutions

---
+++ CS/Problem Solving/Lucky_Detector.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin >> n;
+
+    if(n % 7 == 0){
+        cout<<"Lucky" <<endl;
+    }
+    else{
+        cout << "Not Lucky" << endl;
+    }
+    return 0;
+}
