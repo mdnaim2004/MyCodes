ffd926f0238747f65583c08aef59cffb7d676998
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 11:01:51 +0600

    Refactor pile function and main loop for improved readability

--- CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
+++ CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 bool pile(string s1, string s2){
 
-    if(s1.size() != s2.size())
-        return false;
+    if(s1.size() != s2.size()) return false;
 
     sort(s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
@@ -20,10 +19,8 @@ int main(){
         string s1, s2;
         cin >> s1 >> s2;
 
-        if(pile(s1, s2))
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        if(pile(s1, s2)) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     return 0;
 }
