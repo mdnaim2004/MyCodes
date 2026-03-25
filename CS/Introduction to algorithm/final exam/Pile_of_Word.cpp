5c105ffdc7b6a029bdf13ed58d78363ab714d9a7
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 11:01:15 +0600

    Simplify pile function by returning comparison result directly

--- CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
+++ CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
@@ -9,10 +9,7 @@ bool pile(string s1, string s2){
     sort(s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
 
-    if(s1 == s2)
-        return true;
-    else
-        return false;
+    return (s1 == s2);
 }
 
 int main(){
