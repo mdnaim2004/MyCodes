eec47e974d6c6ce336887b1929953a90c0e16ce3
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 21:45:11 +0600

    this is worng answer, and this answer is comlately wrang

--- CS/Problem Solving/First_Last.cpp
+++ CS/Problem Solving/First_Last.cpp
@@ -6,9 +6,34 @@ int main(){
     cin >> n;
     vector<int>arr(n);
 
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
         cin >> arr[i];
     }
-    
+
+    vector<pair<int, int>> v;
+
+    for(int i=1; i<=v.size(); i++){
+        v.push_back({arr[i], i+1});
+    }
+    sort(v.begin(), v.end());
+
+    int i=1;
+
+    while(i < v.size()){
+        int val = v[i].first;
+        int fst = v[i].second;
+        int lst = v[i].second;
+        
+        int j = i;
+
+        while(j < v.size() && v[i].first == val){
+            lst = v[j].second;
+            j++;
+        }
+
+        cout << val << " " << fst << " " << lst << endl;
+        i = j;
+
+    }
     return 0;
 }
