a05528339af2b9923771c6318f8812d753a97d01
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 11:00:12 +0600

    Refactor pile function to return false for unequal string sizes

--- CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
+++ CS/Introduction to algorithm/final exam/Pile_of_Word.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 bool pile(string s1, string s2){
 
+    if(s1.size() != s2.size())
+        return false;
+
     sort(s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
 
