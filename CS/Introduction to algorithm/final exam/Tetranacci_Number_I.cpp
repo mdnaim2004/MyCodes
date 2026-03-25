ac1cdcdaca50a4e8de59aeeb1a7c7476dfb10839
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 11:03:24 +0600

    Refactor treta function for improved readability

--- CS/Introduction to algorithm/final exam/Tetranacci_Number_I.cpp
+++ CS/Introduction to algorithm/final exam/Tetranacci_Number_I.cpp
@@ -5,17 +5,12 @@ int dp[1005];
 
 int treta(int n){
     
-    if(n == 0)
-        return n;
-    if(n == 1)
-        return 1;
-    if(n == 2)
-        return 1;
-    if(n == 3)
-        return 2;
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 1;
+    if(n == 3) return 2;
 
-    if(dp[n] != -1)
-        return dp[n];
+    if(dp[n] != -1) return dp[n];
    
     return dp[n] = treta(n-1) + treta(n-2) + treta(n-3)+ treta(n-4);
 }
