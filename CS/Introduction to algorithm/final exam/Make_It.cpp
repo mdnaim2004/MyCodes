ba108237dd0026dc879fa62d717c7543892bfe00
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-25 11:04:52 +0600

    Refactor rec and main functions for improved readability

--- CS/Introduction to algorithm/final exam/Make_It.cpp
+++ CS/Introduction to algorithm/final exam/Make_It.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int dp[100005];
 
 bool rec(int cur, int tar){
-    if(cur == tar)
-        return true;
-    if(cur > tar) 
-        return false;
+    if(cur == tar) return true;
+    if(cur > tar) return false;
 
-    if(dp[cur] != -1)
-        return dp[cur];
+    if(dp[cur] != -1) return dp[cur];
 
     bool opt1 = rec(cur+3, tar);
     bool opt2 = rec(cur*2, tar);
@@ -28,10 +25,8 @@ int main(){
         int n;
         cin >> n;
 
-        if(rec(1,n) == true)
-            cout<<"YES"<< endl;
-        else
-            cout<<"NO" << endl;
+        if(rec(1,n) == true) cout<<"YES"<< endl;
+        else cout<<"NO" << endl;
     }
     return 0;
 }
